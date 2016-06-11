9ce888d82f4f5decd03761013b8626e26bae3464
Kimberly Zai <kimberlyz621@gmail.com>
2016-06-10 23:46:09 -0700

    Made some changes to nameTextField

--- Remday/BirthdayViewController.swift
+++ Remday/BirthdayViewController.swift
@@ -43,13 +43,22 @@ class BirthdayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
+        // Enable the Save button only if the text field has a valid Meal name.
+        checkValidBirthdayName()
+        
         // Connect data
         picker.delegate = self
         picker.dataSource = self
         
+        
+        // Display image as a circle
         photoImageView.layer.cornerRadius = photoImageView.frame.size.width / 2;
         photoImageView.clipsToBounds = true;
         
+        // Create white border around image
         photoImageView.layer.borderWidth = 3.5
         photoImageView.layer.borderColor = UIColor.whiteColor().CGColor
         
@@ -78,6 +87,11 @@ class BirthdayViewController: UIViewController {
     }
     
     // MARK: Navigation
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     // This method lets you configure a view controller before it's presented.
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (saveButton === sender) {
@@ -95,47 +109,9 @@ class BirthdayViewController: UIViewController {
     
         // Hide the keyboard.
         nameTextField.resignFirstResponder()
-        
         showPhotoSourceSelection()
     }
 
-    func showPhotoSourceSelection() {
-        // Only show choice to choose between photo library and camera if rear camera is available
-        if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
-            // Allow user to choose between photo library and camera
-            let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
-            
-            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-            alertController.addAction(cancelAction)
-            
-            let cameraAction = UIAlertAction(title: "Take Photo", style: .Default) { (action) in
-                self.showImagePickerController(.Camera)
-            }
-            alertController.addAction(cameraAction)
-            
-            let photoLibraryAction = UIAlertAction(title: "Get Photo From Library", style: .Default) { (action) in
-                self.showImagePickerController(.PhotoLibrary)
-            }
-            
-            alertController.addAction(photoLibraryAction)
-            presentViewController(alertController, animated: true, completion: nil)
-        } else {
-            showImagePickerController(.PhotoLibrary)
-        }
-    }
-    
-    func showImagePickerController(sourceType: UIImagePickerControllerSourceType) {
-        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
-        imagePickerController = UIImagePickerController()
-        
-        // Can either allow user to take photos or select photos from photo library
-        imagePickerController!.sourceType = sourceType
-
-        // Make sure BirthdayViewController is notified when the user picks an image.
-        imagePickerController!.delegate = self
-        
-        presentViewController(imagePickerController!, animated: true, completion: nil)
-    }
 
 }
 
@@ -207,5 +183,71 @@ extension BirthdayViewController: UIImagePickerControllerDelegate, UINavigationC
         
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func showPhotoSourceSelection() {
+        // Only show choice to choose between photo library and camera if rear camera is available
+        if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
+            // Allow user to choose between photo library and camera
+            let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            let cameraAction = UIAlertAction(title: "Take Photo", style: .Default) { (action) in
+                self.showImagePickerController(.Camera)
+            }
+            alertController.addAction(cameraAction)
+            
+            let photoLibraryAction = UIAlertAction(title: "Get Photo From Library", style: .Default) { (action) in
+                self.showImagePickerController(.PhotoLibrary)
+            }
+            
+            alertController.addAction(photoLibraryAction)
+            presentViewController(alertController, animated: true, completion: nil)
+        } else {
+            showImagePickerController(.PhotoLibrary)
+        }
+    }
+    
+    func showImagePickerController(sourceType: UIImagePickerControllerSourceType) {
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        imagePickerController = UIImagePickerController()
+        
+        // Can either allow user to take photos or select photos from photo library
+        imagePickerController!.sourceType = sourceType
+        
+        // Make sure BirthdayViewController is notified when the user picks an image.
+        imagePickerController!.delegate = self
+        
+        presentViewController(imagePickerController!, animated: true, completion: nil)
+    }
+}
+
+extension BirthdayViewController: UITextFieldDelegate {
+    // MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.enabled = false
+    }
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        checkValidBirthdayName()
+        navigationItem.title = textField.text
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func checkValidBirthdayName() {
+        // Disable the Save button if the text field is empty.
+        let text = nameTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+    
+
 }
 
